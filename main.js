89592a1f035ecc1b6eb5909a8c84bc74dd6a4fff
Denisiuk Aleksey <you@example.com>
2020-12-30 17:17:12 +0200

    Add random generate array function

--- main.js
+++ main.js
@@ -21,3 +21,23 @@ function findMaxValue(arr) {
 }
 
 const arr = [12, 15, 20, 25, 59, 79];
+
+function findArithmeticValue(arr) {
+  let popularitySum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    popularitySum += arr[i];
+  }
+  return popularitySum / arr.length;
+}
+
+console.log(Math.ceil(Math.random() * 100));
+
+const arr2 = [];
+
+function createNewRandomArray() {
+  let arr = [];
+  for (i = 0; i < 10; i++) {
+    arr.push(Math.round(Math.random() * 100));
+  }
+  return arr;
+}

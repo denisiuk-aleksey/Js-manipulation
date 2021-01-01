8020ef6ebb839c0bb7e14548f002fb9e54770fa6
Denisiuk Aleksey <you@example.com>
2021-01-01 18:41:32 +0200

    Do some practice tasks

--- main.js
+++ main.js
@@ -39,3 +39,40 @@ function createNewRandomArray() {
   }
   return arr;
 }
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const array3 = array1.concat(array2);
+
+const reverseArray = array1.reverse();
+console.log(reverseArray);
+
+array1.push(4, 5, 6); // добавелние в конец массива
+console.log(array1);
+
+array1.unshift(0); // добавление в начало массива
+console.log(array1);
+
+array1.shift(); // вырезка последнего елемента
+console.log(array1);
+
+array1.pop(); // вырезка 1 елемента
+console.log(array1);
+
+console.log(array1.slice(1)); // новый масив!!!, от исходника старого  
+console.log(array1);
+console.log('Splice');
+console.log(array1.splice(0, 2)); // синтаксис splice(начало сплайса:кол-во елементов)
+console.log(array1);
+
+
+function compareNumbers(a, b) {
+  return a - b; // отображение в человеческом(нормальном порядке), обычно по табличке Unicode
+}
+array1.sort();
+console.log(array1);
+array1.push(10, 20, 34, 11);
+array1.sort(); // unicode
+console.log(array1);
+array1.sort();
+console.log('Сортировка с функцией compareNumbers:', array1.sort(compareNumbers)); // нормальное
